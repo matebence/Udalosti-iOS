7b5e6ad7a59cd1f89385957700a7dd9983e3031c
Bence Mate <ecneb@Bences-Mac.local>
2018-08-04 15:53:36 +0200

    Udalosti pre iOS pre inicializovany

---
+++ Udalosti/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udalosti
+//
+//  Created by Bence Mate on 8/4/18.
+//  Copyright © 2018 Bence Mate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
