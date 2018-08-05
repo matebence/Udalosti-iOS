0c3773feabe222ba94e19fbee9d71278f783e6e5
Bence Mate <ecneb@Bences-Mac.local>
2018-08-05 15:11:50 +0200

    Prihlasenie regitracia UI hotova

---
+++ Udalosti/Autentifikacia/Prihlasenie.swift
@@ -0,0 +1,20 @@
+//
+//  Prihlasenie.swift
+//  Udalosti
+//
+//  Created by Bence Mate on 8/5/18.
+//  Copyright © 2018 Bence Mate. All rights reserved.
+//
+
+import UIKit
+
+class Prihlasenie: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ Udalosti/Autentifikacia/Registracia.swift
@@ -0,0 +1,20 @@
+//
+//  Registracia.swift
+//  Udalosti
+//
+//  Created by Bence Mate on 8/5/18.
+//  Copyright © 2018 Bence Mate. All rights reserved.
+//
+
+import UIKit
+
+class Registracia: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
