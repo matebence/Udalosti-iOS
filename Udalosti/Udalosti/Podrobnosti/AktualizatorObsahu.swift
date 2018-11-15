128a28f54d17447c3bcc95b3fc0dca763fc3cae1
Bence Mate <ecneb@Bences-Mac.local>
2018-11-15 15:23:49 +0100

    model class pre aktualizovanie zaujmov

--- Udalosti/Udalosti/Podrobnosti/AktualizatorObsahu.swift
+++ Udalosti/Udalosti/Podrobnosti/AktualizatorObsahu.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+
+class AktualizatorObsahu{
+    
+    private static var aktualizatorObsahu: AktualizatorObsahu!
+    private var aktualizator: Aktualizator!
+    
+    init() {
+        print("Metoda init bola vykonana")
+        
+    }
+    
+    static func zaujmy() -> AktualizatorObsahu{
+        print("Metoda zaujmy bola vykonana")
+        
+        if(AktualizatorObsahu.aktualizatorObsahu == nil){
+            AktualizatorObsahu.aktualizatorObsahu = AktualizatorObsahu()
+        }
+        
+        return AktualizatorObsahu.aktualizatorObsahu
+    }
+    
+    func nastav(aktualizator: Aktualizator) -> Void{
+        print("Metoda nastav bola vykonana")
+        
+        self.aktualizator = aktualizator
+    }
+    
+    func aktualizuj() -> Void{
+        print("Metoda aktualizuj bola vykonana")
+        
+        self.aktualizator.aktualizujObsahZaujmov();
+    }
+    
+    func hodnota() -> Void{
+        print("Metoda hodnota bola vykonana")
+        
+        if(self.aktualizator != nil){
+            aktualizuj()
+        }
+    }
+}
